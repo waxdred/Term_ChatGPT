f1bec45b5254d97e39d1254f9eef5d040e471fdb
waxcoin <waxcoin@hotmail.com>
2023-01-24 11:42:24 +0700

    Add plugin chatgpt with command ChatGpt

--- lua/chatgpt.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.openChat = function()
-    local api = vim.api
-  local cmd = api.nvim_command
-  local pwd = api.nvim_call_function("getcwd",{})
-  cmd("echom 'Current working directory: "..pwd.."'")
-end
--- plugin/term_chatGPT.lua
+++ plugin/term_chatGPT.lua
@@ -1,3 +1,8 @@
 vim.api.nvim_create_user_command("ChatGPT", function()
-  require("chatgpt").openChat()
+    local api = vim.api
+local cmd = api.nvim_command
+local width = api.nvim_win_get_width(0) * 0.9 -- 90% of the current window width
+local height = api.nvim_win_get_height(0) * 0.9 -- 90% of the current window height
+cmd(string.format("FloatermNew --autoclose=1 --width=%d --height=%d --title=ChatGpt --position=center --wintype=float ~/.local/share/nvim/site/pack/packer/start/Term_ChatGPT/bin/chatGPT",width,height))
+
 end, {})
